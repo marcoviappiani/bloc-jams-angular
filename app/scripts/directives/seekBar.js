b583d599143a05a247cff531aa0db4084ce57b3b
Marco Viappiani <marco.viappiani@gmail.com>
2017-05-15 21:55:03 +0100

    adds jquery dependency

---
+++ app/scripts/directives/seekBar.js
@@ -0,0 +1,18 @@
+ (function() {
+     function seekBar() {
+         
+         return {
+             templateUrl: '/templates/directives/seek_bar.html',
+             replace: true,
+             restrict: 'E',
+             scope: { },
+             link: function(scope, element, attributes) {
+             // directive logic to return
+             }
+         };
+     }
+ 
+     angular
+         .module('blocJams')
+         .directive('seekBar', seekBar);
+ })();
